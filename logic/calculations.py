438564e38fa4db9010b2076a8381e62fc8916088
Con-Adonis <conkavleski@gmail.com>
2025-11-15 15:04:54 -0500

    additions - main file, gemini api, portfolio json

---
+++ logic/calculations.py
@@ -0,0 +1,37 @@
+from google import genai
+import os
+from dotenv import load_dotenv
+import json
+
+with open('portfolio.json') as p:
+    portfolios = json.load(p)
+
+print(portfolios)
+
+load_dotenv()
+api_key = os.getenv("GENAI_API_KEY")
+
+client = genai.Client(api_key="GENAI_API_KEY")
+
+response = client.models.generate_content(
+    model="gemini-2.5-pro",
+    contents='''[SYSTEM PROMPT]Your sole purpose is to read news article information we provide you and generate the likelihood of portfolio affect as well as the sentiment. You will be provided with news article titles and text content. Provide a format as listed below with no other output.[\SYSTEM PROMPT]
+    [PORTFOLIO] ''' + portfolios + ''' [\PORTFOLIO]
+    [NEWS ARTICLE TITLE] [/NEWS ARTICLE TITLE]
+    [NEWS ARTICLE CONTENT] [/NEWS ARTICLE CONTENT]
+    
+    [OUTPUT FORMAT]
+    "<stock_1_ticker>":{
+        "likelihood": <float 0(no effect) - 1(direct effect)>,
+        "sentiment": <float -1(very negative) - 1(very positive)>
+    },
+    "<stock_2_ticker>":{
+        "likelihood": <float 0(no effect) - 1(direct effect)>,
+        "sentiment": <float -1(very negative) - 1(very positive)>
+    },
+    ...
+    [\OUTPUT FORMAT]''',
+)
+
+print(response.content)
+print(response.text)
